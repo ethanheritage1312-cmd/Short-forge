c24ca9a676a030bb752023aaefa8ca08cb8036bb
ethanheritage1312-cmd <ethan.heritage1312@gmail.com>
2026-09-13 07:02:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class VideoRequest(BaseModel):
+    url: str
+
+@app.get("/")
+def home():
+    return {"message": "ShortForge backend is running"}
+
+@app.post("/create-short")
+def create_short(video: VideoRequest):
+    return {
+        "success": True,
+        "message": "Video URL received successfully",
+        "url": video.url
+    }
